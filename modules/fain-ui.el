dcac22749fcb8769913ccf4eaa74c801625b1607
Fikri <fikrirnurhidayat@gmail.com>
2023-05-11 20:30:15 +0700

    feat(ui): remove vertical border

--- modules/fain-ui.el
+++ modules/fain-ui.el
@@ -1,19 +1,19 @@
 ;;; fain-ui.el -*- lexical-binding: t; -*-
 
 (ensure-packages-installed '(doom-themes
-							 vertico
-							 rainbow-mode
-							 rainbow-delimiters
-							 helpful
-							 company
-							 visual-fill-column
+							               vertico
+							               rainbow-mode
+							               rainbow-delimiters
+							               helpful
+							               company
+							               visual-fill-column
                              org-modern
-							 hide-mode-line
-							 mixed-pitch))
+							               hide-mode-line
+							               mixed-pitch))
 
 (setq completion-styles '(substring basic))
 
-(load-theme 'doom-nord-aurora t)
+(load-theme 'modus-operandi t)
 
 (global-visual-line-mode 1)
 (vertico-mode 1)
@@ -24,8 +24,26 @@
 (setq org-modern-list '((43 . "◦") (45 . "•") (42 . "•")))
 
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
-(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+;; (add-hook 'prog-mode-hook #'display-line-numbers-mode)
 (add-hook 'after-init-hook #'global-company-mode)
+
+;; Reseting Borders
+(defun fain/adjust-frame ()
+  "Adjust Emacs frame."
+  (modify-all-frames-parameters
+   '((right-divider-width . 16)
+     (internal-border-width . 16)))
+  (set-face-attribute 'mode-line nil :box nil)          ; Remove border on mode-line
+  (set-face-attribute 'mode-line-inactive nil :box nil) ; Remove border on mode-line-inactive
+  (dolist (face '(window-divider
+                  window-divider-first-pixel
+                  vertical-border
+                  window-divider-last-pixel))
+    (face-spec-reset-face face)
+    (set-face-foreground face (face-attribute 'default :background)))
+  (set-face-background 'fringe (face-attribute 'default :background)))
+
+(add-hook 'window-setup-hook #'fain/adjust-frame)
 
 ;; Helpful
 (define-key helpful-mode-map [remap revert-buffer] #'helpful-update)
